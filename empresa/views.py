60b99718195f00fd1861e79343f6861e690c6dfe
Everton Oliveira <contato.evertonoliveira@hotmail.com>
2022-11-27 22:18:50 -0300

    Criação dos Templates, Statics, Base

---
+++ empresa/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tecnologias
+
+def nova_empresa(request):
+  techs = Tecnologias.objects.all()
+  return render(request, 'nova_empresa.html', {'techs':techs})
