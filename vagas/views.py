2654c56f6578d0bbc83412555531b92d4146c5a7
Everton Oliveira <contato.evertonoliveira@hotmail.com>
2022-12-04 21:04:09 -0300

    Ajustes Models, Retorno de Tarefas

--- vagas/views.py
+++ vagas/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse, Http404
 from django.contrib import messages
 from django.contrib.messages import constants
 from empresa.models import Vagas
+from vagas.models import Tarefa, Emails
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
 
 def nova_vaga(request):
   if request.method == "POST":
@@ -38,7 +43,69 @@ def nova_vaga(request):
 
 def vaga(request, id):
   vaga = get_object_or_404(Vagas, id=id)
-  return render(request, 'vaga.html', {'vaga':vaga})
+  tarefas = Tarefa.objects.filter(vaga=vaga).filter(realizada=False)
+  emails = Emails.objects.filter(vaga=vaga)
+  return render(request, 'vaga.html', {'vaga':vaga, 'tarefas':tarefas, 'emails':emails})
 
 def nova_tarefa(request, id_vaga):
-  pass
+  titulo = request.POST.get('titulo')
+  prioridade = request.POST.get('prioridade')
+  data = request.POST.get('data')
+  #Realizar todas as validações
+  try:
+    tarefa = Tarefa(
+      vaga_id = id_vaga,
+      titulo = titulo,
+      prioridade = prioridade,
+      data = data
+    )
+    
+    tarefa.save()
+    messages.add_message(request, constants.SUCCESS, 'Tarefa adicionada com sucesso!')
+    return redirect(f'/vagas/vaga/{id_vaga}')
+  except:
+    messages.add_message(request, constants.ERROR, 'Erro interno do sistema!!')
+    return redirect(f'/vagas/vaga/{id_vaga}')
+
+def realizar_tarefa(request, id):
+  tarefa_list = Tarefa.objects.filter(id=id).filter(realizada=False)
+  
+  if not tarefa_list.exists():
+    messages.add_message(request, constants.ERROR, 'Realize apenas uma tarefa válida!')
+    return redirect('/home/empresas')
+  
+  tarefa = tarefa_list.first()
+  tarefa.realizada = True
+  tarefa.save()
+  messages.add_message(request, constants.SUCCESS, 'Tarefa realizada com sucesso! Parabéns !!')
+  return redirect(f'/vagas/vaga/{tarefa.vaga.id}')
+
+def envia_email(request, id_vaga):
+  vaga = Vagas.objects.get(id=id_vaga)
+  assunto = request.POST.get('assunto')
+  corpo = request.POST.get('corpo')
+  
+  html_content = render_to_string('emails/template_email.html', {'corpo':corpo})
+  text_content = strip_tags(html_content)
+  email = EmailMultiAlternatives(assunto, text_content, settings.EMAIL_HOST_USER, [vaga.email,])
+  email.attach_alternative(html_content, "text/html")
+  if email.send():
+    mail = Emails(
+      vaga=vaga,
+      assunto=assunto,
+      corpo=corpo,
+      enviado=True
+    )
+    mail.save()
+    messages.add_message(request, constants.SUCCESS, 'Email enviado com sucesso!')
+    return redirect(f'/vagas/vaga/{id_vaga}')
+  else:
+    mail = Emails(
+      vaga=vaga,
+      assunto=assunto,
+      corpo=corpo,
+      enviado=False
+    )
+    mail.save()
+    messages.add_message(request, constants.ERROR, 'Email não enviado. Tente novamente depois!')
+    return redirect(f'/vagas/vaga/{id_vaga}')
